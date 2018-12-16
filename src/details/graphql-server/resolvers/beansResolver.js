d4e310b7585734cf8165bae09088529b38f89b5c
w1nston <wallander.jonas@gmail.com>
2018-12-16 20:17:17 +0100

    Add description to resolvers.

--- src/details/graphql-server/resolvers/beansResolver.js
+++ src/details/graphql-server/resolvers/beansResolver.js
@@ -1,7 +1,12 @@
 'use strict';
 const { listBeans } = require('../../../application/use-case/listBeans');
-const { beansRepositoryFactory } = require('../../../interface-adapters/repository/beansRepositoryMemory');
+const {
+  beansRepositoryFactory,
+} = require('../../../interface-adapters/repository/beansRepositoryMemory');
 
-const beans = () => listBeans(beansRepositoryFactory()).execute();
+const beans = {
+  description: 'Fetches a list of beans.',
+  resolve: () => listBeans(beansRepositoryFactory()).execute(),
+};
 
 module.exports = { beans };
